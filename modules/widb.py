9f5d921db77cf81e1e958afcd8cf0cac79bbd63e
Kitslap <icarsis@gmail.com>
2017-02-17 22:25:04 +0100

    Update widb.py

--- modules/widb.py
+++ modules/widb.py
@@ -1,7 +1,7 @@
 # PROJET PYTHON KAROLAK 2017
 # BURBAUD Pierre - GNERUCCI Maxime
 # Script pour inscrire un fichier csv dans une base de donnée
-# Fonction : Inscrire le retour de readcsv dans la base de donnée
+# Fonction : Inscrire le retour de wil dans la base de donnée
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
