493f2201987e0a6fc9762adeaf813dc407b0b394
Kitslap <icarsis@gmail.com>
2017-02-17 10:32:47 +0100

    Fichiers de bases

---
+++ main.py
@@ -0,0 +1,15 @@
+# PROJET PYTHON KAROLAK 2017
+# BURBAUD Pierre - GNERUCCI Maxime
+# Script pour inscrire un fichier csv dans une base de donnée
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sqlite3
+
+
+# Parcours du fichier ( en ignorant la premiere ligne )
+
+# Ecrire dans la db
+
+# Ecrire ligne par ligne
+    # 
